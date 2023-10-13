9ec98d749e6bf13b2df8535b43dc2d25d6d10647
Michael Lazebny <michael@lazebny.io>
2023-10-13 11:31:47 +0200

    Update ghost

--- dockerfiles/scripts/upload-backup.sh
+++ dockerfiles/scripts/upload-backup.sh
@@ -52,23 +52,7 @@ create_backup() {
   # Skip fail symlinks (because they could point to a volume that is not mounted)
   # Copy ghost content to backup folder
   # Use find to iterate over all files and folders in the source directory
-  find "$content_ghost" -mindepth 1 -depth -print | while read -r item; do
-      # Check if the item is a symlink
-      if [ -L "$item" ]; then
-          # Check if the symlink is valid
-          if [ -e "$item" ]; then
-              # Copy the symlink preserving the link
-              cp -d "$item" "$dest_ghost"
-          else
-              # Log an error message for broken symlinks and continue
-              echo "Warning: Broken symlink skipped: $item" >&2
-              continue
-          fi
-      else
-          # Handle regular files and directories
-          cp -r "$item" "$dest_ghost"
-      fi
-  done
+  find "$content_ghost" -type d -depth 1 -exec cp -r -- '{}' "$dest_ghost" \;
 }
 
 upload_to_s3() {
